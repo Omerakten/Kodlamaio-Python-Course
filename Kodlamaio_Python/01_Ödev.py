09c5995e667b20645f7210ad65fa7075f926ac21
Omerakten <omerfarukakten@hotmail.com>
2023-03-11 16:33:51 +0300

    01-Ödev
    İlk derste verilmiş olan ödev

---
+++ Kodlamaio_Python/01_Ödev.py
@@ -0,0 +1,34 @@
+# 1-)  Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazın
+
+# String Veri Tipi : Metinsel veri tipleri için kullanılır . Örnek = 'Ömer Akten'
+
+# Integer (int) Veri Tipi : Tam sayı değerleri için kullanılır. örnek =150
+
+# Float Veri Tipi : Ondalıklı sayı değerleri için kullanılır. örnek = 3.14
+
+# Boolean Veri Tipi : Mantıksal bir veri tipidir. (true or false)
+
+
+# 2-) Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+
+# String = Eğitmen Adı
+# Integer  : Kursun tamamlanma oranı
+
+
+
+
+# 3-) Kodlama.io sitesinde şart blokları kullnanıldığını düşüdüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+
+
+
+email = input("Mail adresiniz: \n")
+sifre = int(input("Şifreniz: \n"))
+
+
+if email == ("ömerakten@gmail.com") and sifre == (123456):
+    print("Başarıyla giriş yapıldı")
+
+
+else:
+    print("Mail ya da şifre hatali!")
