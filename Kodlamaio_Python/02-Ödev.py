78f6c0495112d2b0be8799aba609e9de31b394f0
Omerakten <omerfarukakten@hotmail.com>
2023-03-11 17:00:15 +0300

    02-Ödev
    1.Gün Ödev 2

---
+++ Kodlamaio_Python/02-Ödev.py
@@ -0,0 +1,29 @@
+ogrenciler = ["ömer","ercan","elif","ceren","selin","berfin"]
+print(ogrenciler)
+print(ogrenciler[0])
+print(ogrenciler[1])
+print(ogrenciler[2])
+print(ogrenciler[3])
+print(ogrenciler[4])
+print(ogrenciler[5])
+print(ogrenciler[::-1]) # sondan başa doğru yazdırmaya yarıyor
+print(ogrenciler[::]) # :: tüm listyeyi yazdırıyor
+print(ogrenciler[3:5]) #3 ve 5. indeks arasında yer alanları yazdırmya yarıyor
+print(ogrenciler[3:]) # 3. indeksten itibaren olan verileri yazdırır
+print(ogrenciler[::2]) # tüm listeyi baştan sona doğru 2 arttırarak yazdırır
+print(ogrenciler[:3]) # 3. indeksten önceki verileri yazdırmaya yarar
+
+
+#ödev2 hepsiburada için ekran görüntüsü aldım gönderdim ayrıca burayada not düşüyorum
+
+evteks = ["Nevresim",
+          "Yorgan",
+          "Yastık",
+          "Battaniye",
+          "Halı &Klim",
+          "Perde",
+          "Alez",
+          "Banyo",
+          "Mutfak",
+          "Kırlent"]
+print(evteks)
